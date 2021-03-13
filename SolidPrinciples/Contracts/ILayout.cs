b89a19ed9d76295ee2f168d17c0e2fd3a469a823
Teddy Aleksov <teddy57603@gmail.com>
2021-03-13 19:20:07 +0200

    Add project files.

---
+++ SolidPrinciples/Contracts/ILayout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidPrinciples.Contracts
+{
+    public interface ILayout
+    {
+        public DateTime Time { get => DateTime.Now; }
+
+
+    }
+}
